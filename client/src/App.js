fe6ac9337434a167a56ca9a6bcda141a935f0dcc
Rubai Bian <lnbyk@udel.edu>
2021-04-03 19:44:16 -0400

    auth page

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,17 @@
-import React from 'react'
-import Particle from './components/Particles'
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import Particle from "./components/Particles";
+import 'antd/dist/antd.css'
+import logo from "./logo.svg";
+import "./App.css";
+import AuthPage from "./pages/user/login/AuthPage";
 
 const App = () => {
-  return <div>
-    <Particle />
-  </div>
-}
+  return (
+    <div>
+      <Particle />
+      <AuthPage />
+    </div>
+  );
+};
 
-
-export default App
+export default App;
